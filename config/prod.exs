ea1dbf2615d4b1eec703f1c12d96e147ad56c254
Jannik Becher <becher.jannik@gmail.com>
2021-05-26 13:22:55 +0200

    update domain

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :corona_vacc_reg, CoronaVaccRegWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "becher.uber.space", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
