453bf88eb800ff1f7bbc62f58c2f047b20db3386
Jannik Becher <becher.jannik@gmail.com>
2021-05-31 13:15:13 +0200

    fix email settings

---
+++ config/runtime.exs
@@ -0,0 +1,14 @@
+import Config
+
+# Config for bamboo mailer
+config :corona_vacc_reg, CoronaVaccReg.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "smtp.ionos.com",
+  hostname: "waesterpraxis.de",
+  port: 587,
+  username: "kontakt@waesterpraxis.de",
+  password: System.fetch_env!("EMAIL_PASSWORD"),
+  tls: :always,
+  retries: 1,
+  no_mx_lookups: false,
+  auth: :if_available
